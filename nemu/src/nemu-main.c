b38f340c089e36a792fbc1231d4b729241a93b3a
tracer-ics2022 <tracer@njuics.org>
2022-09-18 13:36:26 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:36:26 up  3:52,  1 user,  load average: 0.02, 0.03, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -21,6 +21,7 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
+	printf("hello!!!");
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
