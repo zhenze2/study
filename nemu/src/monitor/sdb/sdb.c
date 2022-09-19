7f09bc730caeb5069f85443d5692a787bd88a57d
tracer-ics2022 <tracer@njuics.org>
2022-09-19 23:55:14 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:55:14 up 11:11,  1 user,  load average: 0.43, 0.17, 0.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -83,7 +83,10 @@ static int cmd_x(char *args) {
 	uint32_t sec;
 	sscanf(strtok(NULL, " "),"%x",&sec);
 	for(int i=0;i<fir;i++){
-	printf("0x%08x ", paddr_read(sec,4));
+	if(i%4==0){
+	printf("\n0x%08x:\t", sec);
+	}
+	printf("0x%08x\t", paddr_read(sec,4));
 	sec+=4;
 	}
 	printf("\n");
