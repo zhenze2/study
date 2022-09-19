67e383f9ebc62e2c4d7d6dba92c3123e91afcb91
tracer-ics2022 <tracer@njuics.org>
2022-09-19 18:04:05 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:04:05 up  5:19,  1 user,  load average: 0.04, 0.05, 0.01

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,7 +24,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-	printf("cpu.pc: 0x%08x\ncpu.gpr[32]: \n",cpu.pc);
+	printf("cpu.pc: 0x%08x\ncpu.gpr[32]: \n ",cpu.pc);
 	for(int i=0;i<32;i++){
 	printf("0x%08x\n ",cpu.gpr[i]);
 	}
