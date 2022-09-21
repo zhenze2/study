75608351a7b6231bb97eafee07fe87472fdd4fb0
tracer-ics2022 <tracer@njuics.org>
2022-09-21 23:22:37 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:37 up  2:54,  1 user,  load average: 0.10, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -134,13 +134,13 @@ static bool make_token(char *e) {
         break;
       }
     }
-
+	
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
   }
-
+printf("%d",nr_token);
   return true;
 }
 
