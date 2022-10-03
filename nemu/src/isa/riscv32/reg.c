fc7b2468edb67714d1e2ca723b9bf8778ca89430
tracer-ics2022 <tracer@njuics.org>
2022-10-03 23:08:36 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:08:36 up  4:18,  1 user,  load average: 0.25, 0.12, 0.03

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -30,7 +30,7 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-     	word_t data=0;
+     	word_t data=1;
      	int len=strlen(s);
      	int flag=0;
      	//bool a=true;
