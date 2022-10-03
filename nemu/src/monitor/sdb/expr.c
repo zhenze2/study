cdc2fb8fb9ecd1c7cca6372bdefcdcbd89aaa9d2
tracer-ics2022 <tracer@njuics.org>
2022-10-03 14:16:43 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:43 up  1:33,  1 user,  load average: 0.03, 0.08, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -190,7 +190,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 
-    printf("%u\n",eval(0,nr_token-1));
+    //printf("%u\n",eval(0,nr_token-1));
   return 0;
 }
 
