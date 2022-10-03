f9fa3dc5617c886232f5914526f0f8e1e45117c8
tracer-ics2022 <tracer@njuics.org>
2022-10-03 20:41:53 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:41:53 up  1:51,  1 user,  load average: 0.26, 0.09, 0.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -109,7 +109,7 @@ static int cmd_w(char *args) {
 	new->exp=args;
 	new->past_val=expr(args,false);
 	new->now_val=expr(args,false);
-	printf("watchpoint %d: %s",new->NO,new->exp);
+	printf("watchpoint %d: %s\n",new->NO,new->exp);
   return 0;
 }
 
