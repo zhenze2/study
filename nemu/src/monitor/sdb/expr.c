40dd4302773a1e8911fd4d2a80c7083a44b2e6c4
tracer-ics2022 <tracer@njuics.org>
2022-10-03 23:32:28 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:32:28 up  4:42,  1 user,  load average: 0.03, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -364,12 +364,12 @@ uint32_t eval(int p, int q)
             return val1 + val2;
         case '-':
             return val1 - val2;
-        case DEREF:
-            return paddr_read(val2,4);
         case '*':
             return val1 * val2;
         case '/':
             return val1 / val2;
+        case DEREF:
+            return paddr_read(val2,4);
         case TK_EQ:
             return val1 == val2;
         case TK_INEQ:
