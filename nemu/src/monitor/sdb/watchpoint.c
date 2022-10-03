240428756bab69e9573a321a31fb9bd02dfe2b6e
tracer-ics2022 <tracer@njuics.org>
2022-10-03 19:52:56 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:52:56 up  1:02,  1 user,  load average: 0.16, 0.09, 0.05

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -18,8 +18,8 @@
 #define NR_WP 32
 
 
-static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
+/*static*/ WP wp_pool[NR_WP] = {};
+/*static*/ WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
   int i;
