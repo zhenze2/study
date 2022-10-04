567da3bfdc1ed4501140c158a13ad429ea3d743b
tracer-ics2022 <tracer@njuics.org>
2022-10-04 23:50:44 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:50:44 up 11:24,  1 user,  load average: 0.00, 0.06, 0.05

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,7 +31,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
 		printf("%s\n",s);
-//int suc=0;
+///int suc=0;
      	for(int i=0;i<32;i++){
 	//printf("%d\n",strcmp(s,regs[i]));
 	       	 if(strcmp(s,regs[i])==0){
@@ -41,7 +41,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 	       }
 
 	}
-	printf("NO\n");
+	//printf("NO\n");
 	//assert(suc==1);
 	return 0;
      
