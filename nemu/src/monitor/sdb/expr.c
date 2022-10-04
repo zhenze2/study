7d24e35d4d0968ae4ddd3aac65bc46af1e6971b4
tracer-ics2022 <tracer@njuics.org>
2022-10-04 16:14:18 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:14:18 up  3:47,  1 user,  load average: 0.11, 0.07, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -313,7 +313,7 @@ int num(int c)
 }
 int eval(int p, int q)
 {
-    //printf("%d,%d,%d,%d\n",p,q,check_parentheses(p, q),check_parentheses(p+1, q-1));
+    printf("%d,%d,%d,%d\n",p,q,check_parentheses(p, q),check_parentheses(p+1, q-1));
     if (p > q)
     {
         printf("This is a bad expression\n");
