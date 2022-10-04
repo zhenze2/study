bfbf6925ce8386e923a09186c5c09cb76ff794b5
tracer-ics2022 <tracer@njuics.org>
2022-10-04 12:41:46 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:41:46 up 15 min,  1 user,  load average: 0.08, 0.04, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -334,7 +334,7 @@ uint32_t eval(int p, int q)
         /* The expression is surrounded by a matched pair of parentheses.
          * If that is the case, just throw away the parentheses.
          */
-        
+        printf("YES\n");
         return eval(p + 1, q - 1);
     }
     else
