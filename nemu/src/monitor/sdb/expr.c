fe03b8646f5333995fd19816146fe5f0000f8551
tracer-ics2022 <tracer@njuics.org>
2022-10-04 16:22:14 +0800

    >  compile NEMU 211300046 胡涛 Linux hutao-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:14 up  3:55,  1 user,  load average: 0.23, 0.12, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -350,7 +350,7 @@ int eval(int p, int q)
     {
         /* We should do more things here. */
         int op = oprand(p, q);
-        printf("%d\n",op);
+        //printf("%d\n",op);
         
         int val1=0;
         int val2=0;
@@ -373,7 +373,7 @@ int eval(int p, int q)
         case DEREF:
             return paddr_read(val2,4);
         case MINUS:
-            return -val2;
+            return val2;
         case '*':
             return val1 * val2;
         case '/':
